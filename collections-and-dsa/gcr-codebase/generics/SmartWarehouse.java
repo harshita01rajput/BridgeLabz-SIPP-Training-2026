8b0f9c9f79afcdb63fb0edf8d8f2e604c880ab69
harshita01rajput <rajputharshita010@gmail.com>
2026-07-07 12:29:01 +0530

    harshita: add generics

---
+++ collections-and-dsa/gcr-codebase/generics/SmartWarehouse.java
@@ -0,0 +1,78 @@
+import java.util.ArrayList;
+import java.util.List;
+
+abstract class WarehouseItem {
+    String name;
+
+    WarehouseItem(String name) {
+        this.name = name;
+    }
+
+    public String toString() {
+        return name;
+    }
+}
+
+class Electronics extends WarehouseItem {
+    Electronics(String name) {
+        super(name);
+    }
+}
+
+class Groceries extends WarehouseItem {
+    Groceries(String name) {
+        super(name);
+    }
+}
+
+class Furniture extends WarehouseItem {
+    Furniture(String name) {
+        super(name);
+    }
+}
+
+class Storage<T extends WarehouseItem> {
+
+    private List<T> items = new ArrayList<>();
+
+    public void addItem(T item) {
+        items.add(item);
+    }
+
+    public List<T> getItems() {
+        return items;
+    }
+}
+
+public class SmartWarehouse {
+
+    public static void displayItems(List<? extends WarehouseItem> items) {
+        for (WarehouseItem item : items) {
+            System.out.println(item);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Storage<Electronics> electronicsStorage = new Storage<>();
+        electronicsStorage.addItem(new Electronics("Laptop"));
+        electronicsStorage.addItem(new Electronics("Mobile"));
+
+        Storage<Groceries> groceryStorage = new Storage<>();
+        groceryStorage.addItem(new Groceries("Rice"));
+        groceryStorage.addItem(new Groceries("Sugar"));
+
+        Storage<Furniture> furnitureStorage = new Storage<>();
+        furnitureStorage.addItem(new Furniture("Chair"));
+        furnitureStorage.addItem(new Furniture("Table"));
+
+        System.out.println("Electronics:");
+        displayItems(electronicsStorage.getItems());
+
+        System.out.println("\nGroceries:");
+        displayItems(groceryStorage.getItems());
+
+        System.out.println("\nFurniture:");
+        displayItems(furnitureStorage.getItems());
+    }
+}
