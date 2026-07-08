bf87f8da45dd17aa695ff9c0cd60c2cb1b41e89c
harshita01rajput <rajputharshita010@gmail.com>
2026-07-08 12:23:45 +0530

    updated work

---
+++ collections-and-dsa/binary-search/RotationPoint.java
@@ -0,0 +1,31 @@
+public class RotationPoint {
+
+    public static int findRotationPoint(int[] arr) {
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] > arr[right]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        return left;   // Rotation point ka index
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {6, 7, 8, 9, 1, 2, 3, 4, 5};
+
+        int index = findRotationPoint(arr);
+
+        System.out.println("Rotation Point Index: " + index);
+        System.out.println("Smallest Element: " + arr[index]);
+    }
+}
